c6a72886c28986cc722f7ec94f19c9f4507f0260
moaaztaha <moaaztaha7@gmail.com>
2023-05-23 19:32:09 +0300

    added bot code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Telegram-bot-using-Streamlit-Cloud
+A simple python telegram bot that can be deployed directly using streamlit cloud.
---
+++ bot.py
@@ -0,0 +1,15 @@
+import os
+import streamlit as st
+import telebot
+
+
+bot = telebot.TeleBot(os.environ['SIMPLE_EGY_TOK'])
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+print("Bot is running...")
+bot.polling()
